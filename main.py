40cbf616c61e8c234ec4ad8b4ab80e646a08cf5f
k4my <65527834+k4my-git@users.noreply.github.com>
2023-03-12 01:24:08 +0900

    update

--- main.py
+++ main.py
@@ -2,25 +2,21 @@
 
 import os
 import discord
-from discord.ext import commands
+from discord import app_commands
 
+guild = discord.Object(617002556740206623)
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
+tree = app_commands.CommandTree(bot)
 
 @bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+async def on_ready(self):
+    print(f'{self.user}#{self.user.id} LOGIN!')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith('test'):
+        await message.channel.send('ok')
 
 bot.run(os.environ["DISCORD_TOKEN"])
