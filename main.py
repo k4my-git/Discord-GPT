7fb7dd8c5cf6361c35002f3afbbbad9abac07713
k4my-git <kambara0229@icloud.com>
2023-03-12 14:53:51 +0900

    update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
 import openai
+import asyncio
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
@@ -45,6 +44,38 @@ async def on_message(message):
         embed.set_image(url=image_url)
         
         await message.channel.send(embed=embed)
+    
+    if message.content.startswith('#play'):
+        voice_channel = message.author.voice.channel
+
+        if voice_channel:
+            voice_client = await voice_channel.connect()
+
+            try:
+                attachment = message.attachments[0]
+                file_name = attachment.filename
+
+                # Download the file
+                await attachment.save(file_name)
+
+                # Play the audio file
+                source = discord.FFmpegPCMAudio(file_name)
+                voice_client.play(source)
+
+                # Wait for the audio to finish playing
+                while voice_client.is_playing():
+                    await asyncio.sleep(1)
+
+                # Disconnect from the voice channel
+                await voice_client.disconnect()
+
+                # Remove the audio file from the local filesystem
+                os.remove(file_name)
+
+            except IndexError:
+                await message.channel.send("No audio file found as attachment.")
+        else:
+            await message.channel.send("You must be in a voice channel to use this command.")
         
 
 client.run(os.environ["DISCORD_TOKEN"])
